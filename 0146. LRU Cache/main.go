ac72914dae50e2dd3e0a03609a49ee36799b3238
Mikhail Zvonov <naramanc@gmail.com>
2022-08-15 13:55:58 +0300

    0146 LRU

---
+++ 0146. LRU Cache/main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"container/list"
+	"fmt"
+	"sync"
+)
+
+type Item struct {
+	uid   int
+	value int
+	pos   *list.Element
+}
+
+type LRUCache struct {
+	mu       sync.RWMutex
+	hashmap  map[int]*Item
+	list     *list.List
+	capacity int
+}
+
+func Constructor(capacity int) LRUCache {
+	return LRUCache{
+		hashmap:  make(map[int]*Item, capacity),
+		list:     list.New(),
+		capacity: capacity,
+	}
+}
+
+func (this *LRUCache) Put(key int, value int) {
+	this.mu.Lock()
+	defer this.mu.Unlock()
+
+	newItem := &Item{
+		uid:   key,
+		value: value,
+		pos:   nil,
+	}
+
+	//if item is already in cache
+	if item, ok := this.hashmap[newItem.uid]; ok {
+		//delete from list and hashmap
+		this.list.Remove(item.pos)
+		delete(this.hashmap, item.uid)
+
+		//insert into end of list and update into hm
+		newItem.pos = this.list.PushBack(newItem.uid)
+		this.hashmap[newItem.uid] = newItem
+		return
+	}
+
+	if len(this.hashmap) >= this.capacity {
+		f := this.list.Front()
+		this.list.Remove(f)
+		delete(this.hashmap, f.Value.(int))
+	}
+
+	newItem.pos = this.list.PushBack(newItem.uid)
+	this.hashmap[newItem.uid] = newItem
+}
+
+func (this *LRUCache) Get(key int) int {
+	this.mu.Lock()
+	defer this.mu.Unlock()
+
+	if item, ok := this.hashmap[key]; ok {
+		//update pos
+		this.list.Remove(item.pos)
+		delete(this.hashmap, item.uid)
+
+		//insert
+		item.pos = this.list.PushBack(item.uid)
+		this.hashmap[item.uid] = item
+
+		return item.value
+	}
+
+	return -1
+}
+
+func main() {
+
+	obj := Constructor(2)
+	obj.Put(2, 1)
+	obj.Put(2, 2)
+	fmt.Println(obj.Get(2))
+
+}
